461307af059c9bbcee3bb38e2dd351ba390df6f9
ttdesign <tkthedesigner@gmail.com>
2020-08-23 20:54:05 -0700

    function to calculate tax

--- src/components/Base.js
+++ src/components/Base.js
@@ -8,19 +8,33 @@ function Base() {
 	const [FinalTakeHome, setFinalTakeHome] = useState([]);
     const [socialSec, setSocialSec] = useState([]);
     const [taxRate,setTaxRate] = useState([])
+    const [input1,setInput1] = useState([])
 
 	function handleChangeIncome(event) {
 		setIncome(event.target.value);
 	}
 	function handleChangeDeduction(event) {
 		setDeduction(event.target.value);
-	}
+    }
+    function handleChange(event) {
+        setInput1(event.target.value)
+    }
+    function handleSubmit(event) {
+        event.preventDefault();
+       console.log(input1)
+    }
 	function handleCalculateTax(event) {
 		event.preventDefault();
 		let takeHome;
 		let fica;
-		let grossIncome = income - deduction;
-		setTaxableIncome(grossIncome);
+        let grossIncome = income - deduction;
+        if(grossIncome <0) {
+            setTaxableIncome(income);
+
+        } else if (grossIncome >0){
+            setTaxableIncome(grossIncome)
+        }
+		
 		let medicare = 0.0145 * grossIncome;
 		let bracket1 = 9700;
 		let bracket2 = 39475 - 9700;
@@ -52,7 +66,7 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            //setTaxRate((tax/income)*100)
+            setTaxRate(((income-takeHome)/income)*100)
 		} else if (grossIncome > 204100 && grossIncome <= 510300) {
 			bracket6 = grossIncome - 204100;
 			fica = 8537.4;
@@ -69,7 +83,7 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            //setTaxRate((tax / income) * 100);
+            setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 160725 && grossIncome <= 204100) {
 			bracket5 = grossIncome - 160725;
 			fica = 8537.4;
@@ -85,7 +99,7 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            setTaxRate((tax / income) * 100);
+            setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 84200 && grossIncome <= 160725) {
 			if (grossIncome >= 137700) {
 				fica = 8537.4;
@@ -104,7 +118,7 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            //setTaxRate((tax / income) * 100);
+            setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 39475 && grossIncome <= 84200) {
 			bracket3 = grossIncome - 39475;
 			fica = grossIncome * 0.062;
@@ -116,7 +130,7 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            //setTaxRate((tax / income) * 100);
+            setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 9700 && grossIncome <= 39475) {
 			bracket2 = grossIncome - 9700;
 			fica = grossIncome * 0.062;
@@ -125,18 +139,17 @@ function Base() {
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            //setTaxRate((tax / income) * 100);
+            setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome < 0){
 			fica = 0.062 * grossIncome;
 			takeHome = income - fica - medicare - (bracket1 * taxRate1);
 			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
-            setTaxRate((tax / income) * 100);
-            console.log(taxRate)
+            setTaxRate(((income - takeHome) / income) * 100);
+            
         }
-        setTaxRate((tax/income)*100)
-        // console.log(taxRate)
+    console.log(typeof(taxRate))
 	}
 	return (
 		<div>
@@ -147,23 +160,27 @@ function Base() {
 					type='text'
 					onChange={handleChangeIncome}></input>
 				<input
-                required
+					required
 					placeholder='Deduction'
 					type='text'
 					onChange={handleChangeDeduction}></input>
-				<select onChange={handleChangeIncome}>
+
+				<button type='submit'>Calculate </button>
+			</form>
+			<form onSubmit={handleSubmit}>
+				<select onChange={handleChange}>
 					<option value='Income'>Income </option>
 					<option value='Bonus'>Bonus </option>
 					<option value='Cash'>Cash </option>
 				</select>
-				<button type='submit'>Calculate </button>
+                <button type='submit'>Submit</button>
 			</form>
 			<h3> Break Down</h3>
 			<h4> Your Annual Income is ${income}</h4>
 			<p>Your deduction is ${deduction}</p>
 			<p>Your Total Taxable Income is ${taxableIncome}</p>
 			<p>Your Take Home is ${FinalTakeHome}</p>
-            <p>Your Social Security Tax is ${socialSec}</p>
+			<p>Your Social Security Tax is ${socialSec}</p>
 			<p>Your Tax burden is ${tax}</p>
 			<p>You effective tax rate is {taxRate}% </p>
 		</div>
