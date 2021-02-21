ea6d380dd604aff9ee95b22127de9dae58c357a6
ttdesign <tkthedesigner@gmail.com>
2021-02-20 22:36:16 -0800

    change tax bracket to 2020

--- src/components/Base.js
+++ src/components/Base.js
@@ -6,7 +6,7 @@ function Base() {
 	const [tax, setTax] = useState([]);
 	const [deduction, setDeduction] = useState([]);
 	const [FinalTakeHome, setFinalTakeHome] = useState([]);
-    const [socialSec, setSocialSec] = useState([]);
+    const [socialSec, setSocialSec] = useState();
     const [taxRate,setTaxRate] = useState([])
     const [input1,setInput1] = useState([])
 
@@ -22,27 +22,40 @@ function Base() {
     function handleSubmit(event) {
         event.preventDefault();
        console.log(input1)
-    }
+	}
+	let fica;
+	let medicare;
+	function handleFica() {
+		if (income >= 137700) {
+			fica = 8537.4;
+			medicare = income * 0.0145
+		} else {
+			fica = income * 0.062;
+			medicare = income * 0.0145;			
+		}
+		return (fica,medicare)
+	}
 	function handleCalculateTax(event) {
 		event.preventDefault();
+		handleFica()
+		setSocialSec(fica);
 		let takeHome;
-		let fica;
         let grossIncome = income - deduction;
-        if(grossIncome <0) {
+        if(grossIncome < 0) {
             setTaxableIncome(income);
 
-        } else if (grossIncome >0){
+        } else if (grossIncome > 0){
             setTaxableIncome(grossIncome)
         }
 		
-		let medicare = 0.0145 * grossIncome;
-		let bracket1 = 9700;
-		let bracket2 = 39475 - 9700;
-		let bracket3 = 84200 - 39475;
-		let bracket4 = 160725 - 84200;
-		let bracket5 = 204100 - 160725;
-		let bracket6 = 510300 - 204100;
-		let bracket7 = grossIncome - 510300;
+		
+		let bracket1 = 9875;
+		let bracket2 = 40125 - 9876;
+		let bracket3 = 85525 - 40126;
+		let bracket4 = 163300 - 85526;
+		let bracket5 = 207350 - 163301;
+		let bracket6 = 518400 - 207351;
+		let bracket7 = grossIncome - 518401;
 		let taxRate1 = 0.1;
 		let taxRate2 = 0.12;
 		let taxRate3 = 0.22;
@@ -51,7 +64,6 @@ function Base() {
 		let taxRate6 = 0.35;
 		let taxRate7 = 0.37;
 		if (grossIncome > 510300) {
-			fica = 8537.4;
 			takeHome =
 				grossIncome -
 				fica -
@@ -63,13 +75,11 @@ function Base() {
 					bracket3 * taxRate3 +
 					bracket2 * taxRate2 +
 					bracket1 * taxRate1);
-			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income-takeHome)/income)*100)
 		} else if (grossIncome > 204100 && grossIncome <= 510300) {
 			bracket6 = grossIncome - 204100;
-			fica = 8537.4;
 			takeHome =
 				grossIncome -
 				fica -
@@ -80,13 +90,11 @@ function Base() {
 					bracket3 * taxRate3 +
 					bracket2 * taxRate2 +
 					bracket1 * taxRate1);
-			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 160725 && grossIncome <= 204100) {
 			bracket5 = grossIncome - 160725;
-			fica = 8537.4;
 			takeHome =
 				grossIncome -
 				fica -
@@ -96,16 +104,10 @@ function Base() {
 					bracket3 * taxRate3 +
 					bracket2 * taxRate2 +
 					bracket1 * taxRate1);
-			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 84200 && grossIncome <= 160725) {
-			if (grossIncome >= 137700) {
-				fica = 8537.4;
-			} else {
-				fica = grossIncome * 0.062;
-			}
 			bracket4 = grossIncome - 84200;
 			takeHome =
 				grossIncome -
@@ -115,35 +117,31 @@ function Base() {
 					bracket3 * taxRate3 +
 					bracket2 * taxRate2 +
 					bracket1 * taxRate1);
-			setSocialSec(fica);
+			
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 39475 && grossIncome <= 84200) {
 			bracket3 = grossIncome - 39475;
-			fica = grossIncome * 0.062;
 			takeHome =
 				grossIncome -
 				fica -
 				medicare -
 				(bracket3 * taxRate3 + bracket2 * taxRate2 + bracket1 * taxRate1);
-			setSocialSec(fica);
+			
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome > 9700 && grossIncome <= 39475) {
 			bracket2 = grossIncome - 9700;
-			fica = grossIncome * 0.062;
 			takeHome =
 				grossIncome - fica - medicare - (bracket2 * taxRate2 + bracket1 * taxRate1);
-			setSocialSec(fica);
+			
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
 		} else if (grossIncome < 0){
-			fica = 0.062 * grossIncome;
 			takeHome = income - fica - medicare - (bracket1 * taxRate1);
-			setSocialSec(fica);
 			setTax(income - takeHome);
             setFinalTakeHome(takeHome);
             setTaxRate(((income - takeHome) / income) * 100);
