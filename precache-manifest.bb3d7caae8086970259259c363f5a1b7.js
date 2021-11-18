d14ecf55533e999701acc2f74cff190c1c040b34
ttdesign <tkthedesigner@gmail.com>
2021-11-17 21:39:06 -0800

    Updates

---
+++ precache-manifest.bb3d7caae8086970259259c363f5a1b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f078b58583c4a85b1662dd2b99081b44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43fef40c3eabf7307ff0",
+    "url": "./static/css/main.f5e8839a.chunk.css"
+  },
+  {
+    "revision": "0d1dbfd7be1dc50655dc",
+    "url": "./static/js/2.578d31f1.chunk.js"
+  },
+  {
+    "revision": "64fe18dcc483c578ae266814472af416",
+    "url": "./static/js/2.578d31f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fef40c3eabf7307ff0",
+    "url": "./static/js/main.c153b4dd.chunk.js"
+  },
+  {
+    "revision": "b2de8ab31a044f51266e",
+    "url": "./static/js/runtime-main.7bdc109b.js"
+  },
+  {
+    "revision": "05ef536c1f2e4e03fdba6fc46057eeb4",
+    "url": "./static/media/TTiD-Wsmall.05ef536c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf181601f23ccaefd566b7c8e483aed.js"
+  "./precache-manifest.bb3d7caae8086970259259c363f5a1b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
